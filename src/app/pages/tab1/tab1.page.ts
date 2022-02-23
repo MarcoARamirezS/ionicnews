3ee460e480ecd5ffaa379128bfe4e8c7d09d1356
Marco Ramirez <marco@functionize.com>
2022-02-23 15:05:51 -0600

    add news to app

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/services/news.service';
-import { NewsResponse } from '../../interfaces';
+import { Article } from '../../interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -9,12 +9,15 @@ import { NewsResponse } from '../../interfaces';
 })
 export class Tab1Page implements OnInit {
 
+  public articles: Article[] = []
+
   constructor( private newService: NewsService ) {}
 
   ngOnInit() {
     this.newService.getTopHeadersLines()
-      .subscribe( (resp: NewsResponse) => {
-        console.log(resp.articles);
+      .subscribe( articles => {
+        // console.log(articles);
+        this.articles.push( ...articles )
       });
   }
 }
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { NewsResponse } from '../interfaces';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const apiKey = environment.apiKey;
 
@@ -11,16 +14,14 @@ const apiKey = environment.apiKey;
 export class NewsService {
   constructor( private http: HttpClient ) { }
 
-  /*
   getTopHeadersLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`);
-  }
-  */
-  getTopHeadersLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=business`, {
+    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=business`, {
       params: {
         apiKey
       }
-    });
+    }).pipe(
+      // map( resp => resp.articles )
+      map( ({ articles }) => articles)
+    );
   }
 }
